9cba7131ef51b913a7272d12ad7d4a42575f496b
buckle2000 <buckle2000@163.com>
2016-03-12 20:08:06 +0800

    nothing

--- source/states/OpeningState.hx
+++ source/states/OpeningState.hx
@@ -1,19 +1,26 @@
 package states;
 
+import openfl.Assets;
 import flixel.FlxState;
 import flixel.text.FlxText;
 
 class OpeningState extends FlxState {
-    var text1: FlxText;
-    var text2: FlxText;
+    public static inline var FontSize: Int = 20;
+    var txt_d: FlxText;
+    var txt_s: FlxText;
+    var txt_erivatives: FlxText;
+    var txt_tudio: FlxText;
+
 
     override public function create(): Void {
-        var a: Int;
-        a = 1 + 2 + (3 + 4);
+        Assets.getFont("assets/font/cambria.ttc");
+        txt_d = new FlxText(0, 0, null, "d", FontSize);
+        txt_s = new FlxText(0, 0, null, "s", FontSize);
+        txt_erivatives = new FlxText(0, 0, null, "erivatives", FontSize);
+        txt_tudio = new FlxText(0, 0, null, "tudio", FontSize);
     }
 
     override public function destroy(): Void {
         super.destroy();
     }
-
 }
